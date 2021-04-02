5da72612c6161b570e626402993df8f9601ac0d3
unknown <karimkgp@gmail.com>
2021-04-02 21:37:38 +0200

    juntar dos arrays

--- ex4.js
+++ ex4.js
@@ -1,43 +1,12 @@
-function VocalConsonant(argument) {
-
-  // define un array de vocales
-  let vocals = ['a', 'e', 'i', 'o', 'u'];
-  // declara una variable de tipo boolean
-  let argumentVocal = false;
-  // convierte el argumento de entrada a minúsculas
-  let stringToCompare = argument.toLowerCase();
-
-  // iterar a través del array
-  for (let index in vocals) {
-    // Compara si el argumento dado coincide con cualquiera de los caracteres presentes en el array de vocales
-    if (stringToCompare === vocals[index]) {
-
-      console.log("He trobat la vocal: " + argument);
-
-      // si el argumento dado es vocal, establezca la variable boolean en verdadera
-      argumentVocal = true;
-      break;
-    }
-  }
-
-  if (!argumentVocal)
-    console.log("He trobat la consonant: " + argument);
-
-  /* if else (!=)
-    console.log(argument+" es un número"); */
-
+const nombre = ["K", "A", "R", "I", "M"];
+for (i = 0; i < nombre.length; i++) {
+  console.log(nombre[i]);
 }
 
-// Llamar a la función pasando diferentes caracteres
-VocalConsonant("K");
+const apellido = ["", "G", "H", "A", "Z", "A", "L"];
+for (i = 0; i < apellido.length; i++) {
+  console.log(apellido[i]);
+}
 
-VocalConsonant('A');
-
-VocalConsonant('R');
-
-VocalConsonant('I');
-
-VocalConsonant('M');
-
-VocalConsonant('6');
-
+const nombreCompleto = nombre.concat(apellido);
+console.log(nombreCompleto);
